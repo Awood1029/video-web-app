df0b7ebbdf8f24097df483afc7a73904a1c4c659
Arthur Wood <cwarthur1029@gmail.com>
2023-01-11 21:47:22 -0500

    Imported assets

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import {ReactComponent as AppLogo} from "../../assets/logo.svg"
 import React from "react";
 
 const Navbar = () => {
 	return (
 		<Fragment>
 			<div>
+				<AppLogo />
 				<h1>Navbar</h1>
 			</div>
 			<Outlet />
